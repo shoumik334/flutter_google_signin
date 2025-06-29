4e95c1530257955621d5f076d4d01261ca758803
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-06-29 13:11:37 +0600

    sign in screen design complete

--- lib/reusable_widgets/reusable_widget.dart
+++ lib/reusable_widgets/reusable_widget.dart
@@ -9,3 +9,32 @@ import 'package:flutter/material.dart';
     
 //   );
 // }
+
+TextField reusableTextField(String text, IconData icon, bool isPasswordType,
+    TextEditingController controller) {
+  return TextField(
+    controller: controller,
+    obscureText: isPasswordType,
+    enableSuggestions: !isPasswordType,
+    autocorrect: !isPasswordType,
+    cursorColor: Colors.white,
+    style: TextStyle(color: Colors.white.withAlpha(150)),
+    decoration: InputDecoration(
+      prefixIcon: Icon(
+        icon,
+        color: Colors.white70,
+      ),
+      labelText: text,
+      labelStyle: TextStyle(color: Colors.white.withAlpha(150)),
+      filled: true,
+      floatingLabelBehavior: FloatingLabelBehavior.never,
+      fillColor: Colors.white.withAlpha(77),
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(width: 0, style: BorderStyle.none)),
+    ),
+    keyboardType: isPasswordType
+        ? TextInputType.visiblePassword
+        : TextInputType.emailAddress,
+  );
+}
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -4,10 +4,14 @@ import 'package:flutter_google_signin/reusable_widgets/reusable_widget.dart';
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
 
+  @override
   _SignInScreenState createState() => _SignInScreenState();
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+
+  final TextEditingController _passwordTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +25,12 @@ class _SignInScreenState extends State<SignInScreen> {
         ),
 
         child: Container(
-          alignment: Alignment.topCenter,
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          
+          //alignment: Alignment.topCenter,
           child: SingleChildScrollView(
+            
             child: Padding(
               padding: EdgeInsets.fromLTRB(
                 20,
@@ -31,15 +39,26 @@ class _SignInScreenState extends State<SignInScreen> {
                 0,
               ),
               child: Column(
-                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                
                 children: [
                   Image.asset(
                     "assets/images/icon1.png",
                     fit: BoxFit.fitWidth,
-                    width: 300,
-                    height: 300,
+                    width: 400,
+                    height: 400,
                     color: Colors.white,
                   ),
+
+                  const SizedBox(height: 30,),
+                  reusableTextField("Enter your Mail", Icons.person_2_outlined, false, _emailTextController),
+                  const SizedBox(height: 30,),
+                  reusableTextField("Enter Password", Icons.lock, true, _passwordTextController),
+
+
+
+
                 ],
               ),
             ),
